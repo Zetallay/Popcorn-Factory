5dbcdcdb3a31fadfb30c18619799611f367c45b6
Zetallay <rober898248_@hotmail.com>
2020-03-01 23:01:28 -0700

    Completo

---
+++ PopcomFactory/app/src/main/java/acosta/roberto/popcomfactory/Pelicula.kt
@@ -0,0 +1,3 @@
+package acosta.roberto.popcomfactory
+
+data class Pelicula (var titulo: String, var image: Int, var header:Int, var sinopsis:String)
---
+++ PopcomFactory/app/src/main/java/acosta/roberto/popcomfactory/Serie.kt
@@ -0,0 +1,3 @@
+package acosta.roberto.popcomfactory
+
+data class Serie (var titulo: String, var image: Int, var header:Int, var sinopsis:String)
